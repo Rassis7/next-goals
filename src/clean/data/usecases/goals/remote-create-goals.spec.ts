08424bbafb0abc6b40af8fdc57391825f349cca4
Romulo Assis <romuloassis.as@gmail.com>
2021-03-17 00:24:58 -0300

    test: garantindo que dê erro se o status code for diferente de 200

--- src/clean/data/usecases/goals/remote-create-goals.spec.ts
+++ src/clean/data/usecases/goals/remote-create-goals.spec.ts
@@ -4,6 +4,8 @@ import { Goals } from '@/clean/domain/models';
 import { GoalsParams } from '@/clean/domain/usecases';
 import { HttpPostClientSpy } from '@/clean/data/mocks';
 import { mockGoalsParams, mockGoalsModel } from '@/clean/domain/mocks';
+import { HttpStatusCode } from '@/clean/data/implementations/http/http-response';
+import { InternalServerError, InvalidCredentials } from '@/clean/domain/errors';
 
 const makeSut = (url: string = faker.internet.url()) => {
   const spy = new HttpPostClientSpy<GoalsParams, Goals>();
@@ -26,5 +28,19 @@ describe('CreateGoals', () => {
     await sut.create(mockGoalsParams());
     expect(spy.response.body).toBe(goals);
     expect(spy.response.statusCode).toEqual(200);
+  })
+
+  it('Should return InternalServerError if any error occurs', async () => {
+    const { sut, spy } = makeSut();
+    spy.response.statusCode = HttpStatusCode.serverError
+    const promise = sut.create(mockGoalsParams());
+    await expect(promise).rejects.toThrow(new InternalServerError())
+  })
+
+  it('Should return InvalidCredentials if error 401 occurs', async () => {
+    const { sut, spy } = makeSut();
+    spy.response.statusCode = HttpStatusCode.unauthorized
+    const promise = sut.create(mockGoalsParams());
+    await expect(promise).rejects.toThrow(new InvalidCredentials())
   })
 });
