ed188907dd6f37d2eae9765ee5393f0c1c4bc614
Romulo Assis <romuloassis.as@gmail.com>
2021-03-17 23:44:01 -0300

    feat: implementado verbo post do HttpClient

---
+++ src/clean/infra/http/http-client.ts
@@ -0,0 +1,20 @@
+import { axios, AxiosResponse } from '@/clean/infra/services/axios'
+import {
+  HttpPostClient, HttpPostClientParams, HttpResponse,
+} from '@/clean/data/implementations/http';
+
+export class HttpClient<T=any, R=any> implements HttpPostClient<T, R> {
+  post = async ({ url, body }: HttpPostClientParams<T>): Promise<HttpResponse<R>> => {
+    let httpResponse: AxiosResponse<any>
+    try {
+      httpResponse = await axios.post(url, body)
+    } catch (error) {
+      httpResponse = error.response
+    }
+
+    return {
+      body: httpResponse.data,
+      statusCode: httpResponse.status,
+    }
+  }
+}
---
+++ src/clean/infra/mocks/axios.ts
@@ -0,0 +1,11 @@
+import faker from 'faker'
+import { axios } from '@/clean/infra/services/axios'
+
+export const mockAxios = (): jest.Mocked<typeof axios> => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>
+  mockedAxios.post.mockResolvedValue({
+    data: faker.random.objectElement(),
+    status: faker.random.number(),
+  })
+  return mockedAxios
+}
