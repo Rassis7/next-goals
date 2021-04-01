44ce8dec281ea2b622b914ae65dbcc31a81ce9ec
Romulo Assis <romuloassis.as@gmail.com>
2021-04-01 08:40:06 -0300

    refactor: added react-hooks-form

--- src/clean/presentation/pages/create-goals/components/form/index.tsx
+++ src/clean/presentation/pages/create-goals/components/form/index.tsx
@@ -4,36 +4,41 @@ import {
   Flex,
   Input,
   Textarea,
-  NumberInput,
-  NumberInputField,
-  FormHelperText,
   Box,
 } from '@chakra-ui/react'
+import {
+  FieldValues, useForm,
+} from 'react-hook-form'
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import FormControl from '@/clean/presentation/components/form-control'
-import { FormatCurrency } from '@/clean/presentation/utils/currency'
+import CurrencyInput from '@/clean/presentation/components/currency-input'
+import { goalsValidation } from '@/clean/presentation/validations/goals'
 
 const Form = () => {
   const styles = useStyleConfig('GradientButton')
 
-  const InputNumber = () => {
-    const value = 1050.9
-    const format = (val:number) => FormatCurrency.setValue(val).getBRL().build()
-    const parse = (val: string) => val
-    return (
-      <NumberInput
-        onChange={(valueString) => parse(valueString)}
-        value={format(value)}
-        max={50}
-      >
-        <NumberInputField />
-      </NumberInput>
-    )
+  const {
+    name, amount, createdAt, finishAt, description,
+  } = goalsValidation
+  const {
+    control, handleSubmit, errors,
+  } = useForm({ resolver: yupResolver(
+    yup.object().shape({
+      name, amount, createdAt, finishAt, description,
+    }),
+  ) })
+
+  console.log(errors);
+
+  const onSubmit = (values: FieldValues) => {
+    console.log(values)
   }
 
   return (
     <form
-      data-testeid="form"
-      action=""
+      data-testid="form"
+      onSubmit={handleSubmit(onSubmit)}
     >
       <Flex
         flexDirection="column"
@@ -43,47 +48,73 @@ const Form = () => {
 
         <FormControl
           name="name"
+          control={control}
           label="Sua meta deve ter um nome, que seja simples e claro"
           labelProps={{ textAlign: 'center' }}
-          controlProps={{ isRequired: true }}
+          controlProps={{ isInvalid: errors?.name }}
+          errorMessage={errors?.name?.message}
         >
           <Input
             type="text"
-            isInvalid
           />
         </FormControl>
 
-        <FormControl
-          name="finishAt"
-          label="Toda meta tem um ínicio e fim, defina seus prazos"
-          labelProps={{ textAlign: 'center' }}
-          controlProps={{ mt: '8', isRequired: true }}
+        <Flex
+          justifyContent="space-between"
+          alignItems="center"
+          w="100%"
         >
-          <Flex
-            flexDirection="column"
+          <Box
+            flex={1}
+            mr="1.5"
           >
-            <Box>
-              <Input
-                type="date"
-              />
-              <FormHelperText>Data de Início</FormHelperText>
-            </Box>
-            <Box
-              mt="1rem"
+            <FormControl
+              name="createdAt"
+              control={control}
+              label="Toda meta tem um ínicio"
+              labelProps={{ textAlign: 'center' }}
+              controlProps={{
+                mt: '8', isInvalid: errors?.createdAt,
+              }}
+              helpText="Data de Início"
+              errorMessage={errors?.createdAt?.message}
             >
               <Input
                 type="date"
               />
-              <FormHelperText>Data de Fim</FormHelperText>
-            </Box>
-          </Flex>
-        </FormControl>
+            </FormControl>
+          </Box>
+          <Box
+            flex={1}
+            ml="1.5"
+          >
+            <FormControl
+              name="finishAt"
+              control={control}
+              label="...e também um fim"
+              labelProps={{ textAlign: 'center' }}
+              controlProps={{
+                mt: '8', isInvalid: errors?.finishAt,
+              }}
+              helpText="Data de Fim"
+              errorMessage={errors?.finishAt?.message}
+            >
+              <Input
+                type="date"
+              />
+            </FormControl>
+          </Box>
+        </Flex>
 
         <FormControl
           name="description"
+          control={control}
           label="É legal colocar uma descrição para sua meta, principalmente se for algo motivador"
           labelProps={{ textAlign: 'center' }}
-          controlProps={{ mt: '8' }}
+          controlProps={{
+            mt: '8', isInvalid: !!errors.description,
+          }}
+          errorMessage={errors?.description?.message}
         >
           <Textarea
             rows={5}
@@ -91,15 +122,21 @@ const Form = () => {
         </FormControl>
 
         <FormControl
-          name="description"
+          name="amount"
+          control={control}
           label="Se sua meta for financeira, coloque aqui o valor dela"
           labelProps={{ textAlign: 'center' }}
-          controlProps={{ mt: '8' }}
+          controlProps={{
+            mt: '8', isInvalid: errors?.amount,
+          }}
+          defaultValue={0}
+          errorMessage={errors?.amount?.message}
         >
-          <InputNumber />
+          <CurrencyInput />
         </FormControl>
 
         <Button
+          type="submit"
           title="Próximo campo"
           mt="20"
           sx={styles}
