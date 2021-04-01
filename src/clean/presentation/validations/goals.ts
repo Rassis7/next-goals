a10ee68f79228146d70f027faa0a219af3db096f
Romulo Assis <romuloassis.as@gmail.com>
2021-04-01 08:39:30 -0300

    refactor: added default date validation

--- src/clean/presentation/validations/goals.ts
+++ src/clean/presentation/validations/goals.ts
@@ -1,13 +1,15 @@
 import * as yup from 'yup'
 
-export const GoalsValidation = ({
+export const goalsValidation = {
   name: yup.string().required('O nome é obrigatório'),
   status: yup.mixed()
     .oneOf(['OPENED', 'CLOSED'], 'O status deve ser Aberto ou Fechado')
     .required('O status é obrigatório'),
-  createdAt: yup.date().required('A data de início deve ser informada.'),
-  finishAt: yup.date().required('A data de fim deve ser informada.'),
+  createdAt: yup
+    .date()
+    .required('A data de início deve ser informada.').default(new Date()),
+  finishAt: yup.date().required('A data de fim deve ser informada.').default(new Date()),
   description: yup.string(),
   image: yup.string().url(),
   amount: yup.number().min(0, 'O valor não pode ser negativo'),
-})
+}
